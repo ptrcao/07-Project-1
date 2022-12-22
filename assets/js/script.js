4976bad72bc7e7e43a13049e91d369e81cac85aa
jo <jojovillajuan18@gmail.com>
2022-12-22 21:46:20 +1100

    fuelapi fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,35 @@ var accessToken = "";
 
 function getAccessToken() {
   fetch(
-    "https://api.onegov.nsw.gov.au/oauth/client_credential/accesstoken?grant_type=client_credentials",
-    {
-      headers: {
-        accept: "application/json",
-        authorization:
-          "Basic ODkwd2VhbUxVZWJUMTlqRFJBWGdBTWJoZVJjT2NRcHE6NERGN2RwUkw1Qlo4MERrMQ==",
-      },
-      referrerPolicy: "no-referrer",
-      method: "GET",
-      mode: "cors",
-      credentials: "include",
-    }
+          "https://api.onegov.nsw.gov.au/oauth/client_credential/accesstoken?grant_type=client_credentials",
+          {
+            headers: {
+              accept: "application/json",
+              authorization:
+                "Basic ODkwd2VhbUxVZWJUMTlqRFJBWGdBTWJoZVJjT2NRcHE6NERGN2RwUkw1Qlo4MERrMQ==",
+            },
+    })
+    .then(response => response.json())
+    .then(
+      data => 
+  fetch("https://api.onegov.nsw.gov.au/FuelPriceCheck/v2/fuel/prices/nearby", {
+    "headers": {
+  
+      "apikey": "890weamLUebT19jDRAXgAMbheRcOcQpq",
+      "authorization": `Bearer ${data.access_token}`,
+      "content-type": "application/json",
+      "requesttimestamp": "20/12/2022 08:00:00 PM",
+      "transactionid": "1"
+    },
+  
+    "body": "{\n  \"fueltype\": \"P95\",\n  \"brand\": [\n    \"BP\"\n  ],\n  \"namedlocation\": \"2065\",\n  \"latitude\": \"-33.4362551\",\n  \"longitude\": \"151.2966549\",\n  \"radius\": \"\",\n  \"sortby\": \"price\",\n  \"sortascending\": \"true\"\n}",
+    "method": "POST",
+  
+  })
   )
-    .then((response) => response.json())
-    .then((data) => getPriceNearby(data));
+  getRefData();
   // .then( data => {accessToken = data.access_token; console.log(accessToken)} );
-}
+};
 
 function getRefData() {
   fetch(
@@ -29,10 +41,7 @@ function getRefData() {
       headers: {
         accept: "application/json",
         apikey: "890weamLUebT19jDRAXgAMbheRcOcQpq",
-        authorization: "Bearer GzR6lfpfZ7M4YAslUyVwCq58uGQe",
-        "cache-control": "max-age=0",
-        "content-type": "application/json; charset=utf-8",
-        "if-modified-since": "15/12/2022 01:14:00 PM",
+        authorization: `Bearer ${data.access_token}`,
         requesttimestamp: "22/12/2022 01:14:00 PM",
         transactionid: "4",
       },
@@ -43,25 +52,7 @@ function getRefData() {
   )
     .then((response) => response.json())
     .then((data) => console.log(data));
-}
-
-function getPriceNearby() {
-  fetch("https://api.onegov.nsw.gov.au/FuelPriceCheck/v2/fuel/prices/nearby", {
-    headers: {
-      accept: "application/json",
-      apikey: "890weamLUebT19jDRAXgAMbheRcOcQpq",
-      authorization: "Bearer ZyVqqkuxwGgO10iGmLzKwTKgJ5rl",
-      "content-type": "application/json",
-      requesttimestamp: "22/12/2022 01:04:00 PM",
-      transactionid: "3",
-    },
-    body: '{\n  "fueltype": "P95",\n  "brand": [\n    "BP"\n  ],\n  "namedlocation": "2065",\n  "latitude": "-33.4362551",\n  "longitude": "151.2966549",\n  "radius": "",\n  "sortby": "price",\n  "sortascending": "true"\n}',
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
+};
 
 checkBTN.addEventListener("click", getAccessToken);
+
